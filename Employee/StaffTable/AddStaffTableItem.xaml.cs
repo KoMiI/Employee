4b6ebdef136426df8dac4e7e1be1d38b0c79e873
LearLocker <b.xenia.d-97@yandex.ru>
2020-03-10 14:45:51 +0700

    Add Staff Table Form

---
+++ Employee/StaffTable/AddStaffTableItem.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Employee.StaffTable
+{
+    /// <summary>
+    /// Логика взаимодействия для AddStaffTableItem.xaml
+    /// </summary>
+    public partial class AddStaffTableItem : Window
+    {
+        public AddStaffTableItem()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Employee/StaffTable/StaffTable.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Employee.StaffTable
+{
+    /// <summary>
+    /// Логика взаимодействия для StaffTable.xaml
+    /// </summary>
+    public partial class StaffTable : Window
+    {
+        public StaffTable()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddStaffTableItem addStaffTableItem = new AddStaffTableItem();
+            addStaffTableItem.Show();
+        }
+    }
+}
